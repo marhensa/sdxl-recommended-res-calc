965e36ea9f721ce2f4beba254e858a03ce16b437
marhensa <marhensa@gmail.com>
2023-08-21 00:15:29 +0700

    making it consistent as 9 decimal places (float precision)

--- sdxl-recommended-res-calc.py
+++ sdxl-recommended-res-calc.py
@@ -2,7 +2,8 @@ import os
 
 """
     
-This is the standalone python script, for ComfyUI custom node it's on other file: customnode_sdxl_recommended_res_calc.py
+This is the standalone python script, 
+for ComfyUI custom node it's on other file: customnode_sdxl_recommended_res_calc.py
     
 A simple tool to calculate the recommended initial latent size 
 for SDXL image generation and its Upscale Factor 
@@ -21,55 +22,55 @@ def calculate_aspect_ratios(desiredXSIZE, desiredYSIZE):
     
 # Horizontal aspect ratio
     accepted_ratios_horizontal = {
-    "4:1": (2048, 512, 4.000),
-    "31:8": (1984, 512, 3.875),
-    "15:4": (1920, 512, 3.750),
-    "29:8": (1856, 512, 3.625),
-    "28:9": (1792, 576, 3.111),
-    "3:1": (1728, 576, 3.000),
-    "26:9": (1664, 576, 2.889),
-    "5:2": (1600, 640, 2.502),
-    "12:5": (1536, 640, 2.402),
-    "23:11": (1472, 704, 2.091),
-    "2:1": (1408, 704, 2.002),
-    "21:11": (1344, 704, 1.909),
-    "7:4": (1344, 768, 1.750),
-    "5:3": (1280, 768, 1.667),
-    "19:13": (1216, 832, 1.462),
-    "18:13": (1152, 832, 1.385),
-    "9:7": (1152, 896, 1.286),
-    "17:14": (1088, 896, 1.214),
-    "17:15": (1088, 960, 1.133),
-    "16:15": (1024, 960, 1.067)
+    "4:1": (2048, 512, 4.000000000),
+    "31:8": (1984, 512, 3.875000000),
+    "15:4": (1920, 512, 3.750000000),
+    "29:8": (1856, 512, 3.625000000),
+    "28:9": (1792, 576, 3.111111111),
+    "3:1": (1728, 576, 3.000000000),
+    "26:9": (1664, 576, 2.888888889),
+    "5:2": (1600, 640, 2.500000000),
+    "12:5": (1536, 640, 2.400000000),
+    "23:11": (1472, 704, 2.090909091),
+    "2:1": (1408, 704, 2.000000000),
+    "21:11": (1344, 704, 1.909090909),
+    "7:4": (1344, 768, 1.750000000),
+    "5:3": (1280, 768, 1.666666667),
+    "19:13": (1216, 832, 1.461538462),
+    "18:13": (1152, 832, 1.384615385),
+    "9:7": (1152, 896, 1.285714286),
+    "17:14": (1088, 896, 1.214285714),
+    "17:15": (1088, 960, 1.133333333),
+    "16:15": (1024, 960, 1.066666667)
     }
 
 # Vertical aspect ratio
     accepted_ratios_vertical = {
-    "15:16": (960, 1024, 0.938),
-    "15:17": (960, 1088, 0.882),
-    "14:17": (896, 1088, 0.824),
-    "7:9": (896, 1152, 0.778),
-    "13:18": (832, 1152, 0.722),
-    "13:19": (832, 1216, 0.684),
-    "3:5": (768, 1280, 0.600),
-    "4:7": (768, 1344, 0.571),
-    "11:21": (704, 1344, 0.524),
-    "1:2": (704, 1408, 0.500),
-    "11:23": (704, 1472, 0.478),
-    "5:12": (640, 1536, 0.417),
-    "2:5": (640, 1600, 0.400),
-    "9:26": (576, 1664, 0.346),
-    "1:3": (576, 1728, 0.333),
-    "9:28": (576, 1792, 0.321),
-    "8:29": (512, 1856, 0.276),
-    "4:15": (512, 1920, 0.267),
-    "8:31": (512, 1984, 0.258),
-    "1:4": (512, 2048, 0.250)
+    "15:16": (960, 1024, 0.937500000),
+    "15:17": (960, 1088, 0.882352941),
+    "14:17": (896, 1088, 0.823529412),
+    "7:9": (896, 1152, 0.777777778),
+    "13:18": (832, 1152, 0.722222222),
+    "13:19": (832, 1216, 0.684210526),
+    "3:5": (768, 1280, 0.600000000),
+    "4:7": (768, 1344, 0.571428571),
+    "11:21": (704, 1344, 0.523809524),
+    "1:2": (704, 1408, 0.500000000),
+    "11:23": (704, 1472, 0.478260870),
+    "5:12": (640, 1536, 0.416666667),
+    "2:5": (640, 1600, 0.400000000),
+    "9:26": (576, 1664, 0.346153846),
+    "1:3": (576, 1728, 0.333333333),
+    "9:28": (576, 1792, 0.321428571),
+    "8:29": (512, 1856, 0.275862069),
+    "4:15": (512, 1920, 0.266666667),
+    "8:31": (512, 1984, 0.258064516),
+    "1:4": (512, 2048, 0.250000000)
     }
     
 # Square aspect ratio
     accepted_ratios_square = {
-    "1:1": (1024, 1024, 1.000)
+    "1:1": (1024, 1024, 1.00000000)
     }
 
     
@@ -138,27 +139,27 @@ def calculate_aspect_ratios(desiredXSIZE, desiredYSIZE):
         accepted_size = accepted_XSIZE
         desired_size = desiredXSIZE
         excesstocrop = "Final Height"
-        scaling_factor = round(upscale_factor_width, 3)
+        scaling_factor = round(upscale_factor_width, 9)
     else:
         more_less = "SDXL Height"
         accepted_size = accepted_YSIZE
         desired_size = desiredYSIZE
         excesstocrop = "Final Width"
-        scaling_factor = round(upscale_factor_height, 3)
+        scaling_factor = round(upscale_factor_height, 9)
     
     print("\nScaling factor to reach", desiredXSIZE, "x", desiredYSIZE)
     print("calculated from (", more_less, ") to avoid shortage,")
     print("you can crop (", excesstocrop, ") excess later")
     print(colorGREEN + "Scale factor :", scaling_factor, colorRESET)
 
-    # Calculate the downscale factor from Upscale 4x Node and round to 3 digits
-    downscalefrom2x_factor = round(scaling_factor / 4, 3)
+    # Calculate the downscale factor from Upscale 4x Node and round to 9 digits
+    downscalefrom2x_factor = round(scaling_factor / 4, 9)
     
     print("\nReverse Upscale Factor after using 4x-Upscaler Node = (", scaling_factor, "/ 4 )")
     print(colorGREEN + "Reverse Upscale Factor for 4x-Upscaler :", downscalefrom2x_factor, colorRESET)
     
-    # Calculate the downscale factor from Upscale 2x Node and round to 3 digits
-    downscalefrom4x_factor = round(scaling_factor / 2, 3)
+    # Calculate the downscale factor from Upscale 2x Node and round to 9 digits
+    downscalefrom4x_factor = round(scaling_factor / 2, 9)
     
     print("\nReverse Upscale Factor after using 2x-Upscaler Node = (", scaling_factor, "/ 2 )")
     print(colorGREEN + "Reverse Upscale Factor for 2x-Upscaler :", downscalefrom4x_factor, colorRESET)
